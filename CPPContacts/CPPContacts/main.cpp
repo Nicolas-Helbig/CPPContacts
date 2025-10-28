f934d7fe24f25a6b37544c05de681fc992a75b8b
Blindside Studios <nicolas.helbig@icloud.com>
2025-10-28 17:45:58 +0100

    Created project with Xcode
    Ladies and gentlemen, we have code. None of it was written by me but we have code.

---
+++ CPPContacts/CPPContacts/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPContacts
+//
+//  Created by Nicolas Helbig on 28.10.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
